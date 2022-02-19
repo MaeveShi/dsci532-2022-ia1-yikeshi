8eba7ba591da823aabad04b1b677b93c4c63d1a5
MaeveShi <shiyike0909@gmail.com>
2022-02-18 20:23:01 -0800

    change dataset

--- src/app.py
+++ src/app.py
@@ -4,30 +4,31 @@ from vega_datasets import data
 
 
 # Read in global data
-cars = data.cars()
+iris = data.iris()
 
 # Setup app and layout/frontend
 app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 app.layout = html.Div([
     html.Iframe(
         id='scatter',
-        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+        style={'border-width': '0', 'width': '80%', 'height': '400px'}),
     dcc.Dropdown(
         id='xcol-widget',
         value='Horsepower',  # REQUIRED to show the plot on the first page load
-        options=[{'label': col, 'value': col} for col in cars.columns])])
+        options=[{'label': col, 'value': col} for col in iris.columns[0:4]])])
 
 # Set up callbacks/backend
 @app.callback(
     Output('scatter', 'srcDoc'),
     Input('xcol-widget', 'value'))
 def plot_altair(xcol):
-    chart = alt.Chart(cars).mark_point().encode(
+    chart = alt.Chart(iris).mark_point().encode(
         x=xcol,
-        y='Displacement',
-        tooltip='Horsepower').interactive()
+        y='species',
+        tooltip='sepalLength').interactive()
     return chart.to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
 server = app.server
+
